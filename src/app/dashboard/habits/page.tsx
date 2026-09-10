f0f1d26790d0a6242e98b9c5eba0992d72ec6f56
Dot010 <jonathan2500@outlook.pt>
2026-09-10 18:08:43 -0300

    feat(habits): add HabitPage and NewHabitModal for habit management

---
+++ src/app/dashboard/habits/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useState } from "react";
+import { Habit, mockUser } from "@/data/mockData";
+import { NewHabitModal } from "@/components/NewHabitModal";
+
+
+const HabitPage = () => {
+  const [habits, setHabits] = useState<Habit[]>([...(mockUser.habits || [])]);
+
+  // Função para adicionar um novo hábito
+  const handleAddHabit = (newHabit: Habit) => {
+    setHabits((prevHabits) => [...prevHabits, newHabit]);
+  };
+
+  const handleRemoveHabit = ( id: string) => {
+    setHabits((prevHabits) => prevHabits.filter((habit) => habit.id !== id));
+  }
+
+
+  // Função para alternar o status de um hábito
+const toggleHabit = (id: string) => {
+  setHabits((prevHabits) =>
+    prevHabits.map((habit) =>
+      habit.id === id
+        ? { ...habit, completedToday: !habit.completedToday }
+        : habit
+    )
+  );
+};
+  return (
+    <div className="p-8 space-y-6 max-w-5xl mx-auto bg-slate-50 min-h-screen font-sans">
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-3xl font-bold text-slate-800">Meus Hábitos</h1>
+          <p className="text-sm text-slate-500 mt-1 ">
+            Registre seus hábitos e acompanhe sua evolução
+          </p>
+        </div>
+        <NewHabitModal onAddHabit={handleAddHabit} />
+
+      </div>
+        {/* Contadores */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="p-5 bg-white border border-slate-200 rounded-2xl shadow-sm">
+          <p className="text-xs text-slate-500 uppercase font-bold">Concluidos Hoje</p>
+          <p className="font-bold text-2xl text-slate-800 mt-1 font-black">
+            {habits.filter((h) => h.completedToday).length / habits.length}
+          </p>
+        </div>
+        {/* Taxa de Sucesso */}
+        <div className="p-5 bg-white border border-slate-200 rounded-2xl shadow-sm">
+          <p className="text-xs text-slate-500 uppercase font-bold">Taxa de Sucesso</p>
+          <p className="font-bold text-2xl text-emerald-600 mt-1 font-black">
+            {habits.length > 0
+            ? (habits.filter((h) => h.completedToday).length / habits.length) * 100
+            : 0} %
+          </p>
+        </div>
+{/* Maior Sequencia */}
+         <div className="p-5 bg-white border border-slate-200 rounded-2xl shadow-sm">
+          <p className="text-xs text-slate-500 uppercase font-bold">Maior Sequencia</p>
+          <p className="font-bold text-2xl text-amber-500 mt-1 font-black">
+              🔥 {habits.length > 0 ? Math.max(...habits.map((h) => h.streak), 0) : 0} dias
+          </p>
+        </div>
+        </div>
+
+      <div className="space-y-3">
+        {habits.map((habit) => (
+          <div
+            key={habit.id}
+            className={`p-4 border rounded-xl flex justify-between items-center shadow-sm ${
+              habit.completedToday
+                ? " bg-emerald-50 border-emerald-200"
+                : "bg-white border-slate-200"
+            }`}
+          >
+            <div className="flex flex-col">
+              <span className="text-[10px] font-bold uppercase tracking-wider text-slate-400">
+                {habit.category}
+              </span>
+              <p
+                className={`font-bold text-lg ${habit.completedToday ? "line-through text-slate-400" : "text-slate-800"}`}
+              >
+                {habit.title}
+              </p>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <span
+                className="font-bold text-amber-500 bg-amber-50 px-2 py-2 py-1
+                rounded-md text-sm"
+              >🔥{habit.streak}d</span>
+              <button onClick={() => toggleHabit(habit.id)}
+                className={`w-8 h-8 rounded-full border-2 transition-colors flex items-center justify-center font-bold 
+                ${habit.completedToday ? "bg-emerald-500 border-emerald-500 text-white" : "bg-white border-slate-200 text-slate-500"}}`}
+              >
+                {habit.completedToday ? "👍" : "👎"}
+                
+              </button>
+              
+              <button onClick={() => handleRemoveHabit(habit.id)} className="w-8 h-8 rounded-full border-2 flex items-center justify-center font-bold text-slate-500 border-slate-200">
+                🗑️
+              </button>
+
+            </div>
+            
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HabitPage;
---
+++ src/components/NewHabitModal.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useState } from "react";
+import { Plus } from "lucide-react";
+import { Habit } from "@/data/mockData";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { title } from "process";
+
+interface NewHabitModalProps {
+  onAddHabit: (habit: Habit) => void;
+}
+
+export function NewHabitModal({ onAddHabit }: NewHabitModalProps) {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("Saude");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+
+    onAddHabit({
+      id: Date.now().toString(),
+      title,
+      category,
+      completedToday: false,
+      streak: 0,
+      description: undefined,
+      name: undefined
+    });
+
+    setTitle("");
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className="bg-emerald-600 hover:bg-emerald-700 text-white gap-2">
+          <Plus className="h-4 w-4" /> Novo Hábito
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Criar Novo Hábito</DialogTitle>
+        </DialogHeader>
+
+        <form
+          id="habit-form"
+          onSubmit={handleSubmit}
+          className="space-y-4 pt-2"
+        >
+          <div className="space-y-2">
+            <label className="text-xs font-semibold uppercase text-slate-500">
+              Nome do Hábito
+            </label>
+            <Input
+              required
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="EX: Meditar, Ler, Exercícios"
+            />
+          </div>
+
+          <div className="space-y-2">
+            <label className="text-xs font-semibold upercase text-slate-500">
+              Categoria
+            </label>
+            <select
+              required
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+                          className="w-full h-10 px-3 rounded-md border border-slate-200 bg-white text-sm focus:outline-none
+              focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            >
+              <option value="Saude">Saúde</option>
+              <option value="Estudo">Estudo</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Lazer">Lazer</option>
+            </select>
+          </div>
+          <DialogFooter className="pt-4">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => setOpen (false)}
+            >
+          Cancelar
+                      </Button>
+                      <Button type="submit" className="bg-emerald-600 hover:bg-emerald-700 text-white">
+              Salvar Hábito
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
